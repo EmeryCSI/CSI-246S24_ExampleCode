acb87a004498caf54a97e64043e96f67c1ce80ee
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-05-21 08:44:50 -0700

    Updated

--- Week7/2_customHooks/src/components/ThemeSelector.jsx
+++ Week7/2_customHooks/src/components/ThemeSelector.jsx
@@ -2,11 +2,14 @@ import useLocalStorage from "../hooks/useLocalStorage";
 function ThemeSelector() {
   //bring in our custom hook
   const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  const orangeTheme = () => "orange";
   return (
     <>
       <h2>Theme Selector</h2>
       <button onClick={() => setTheme("light")}>Light Theme</button>
       <button onClick={() => setTheme("dark")}>Dark Theme</button>
+      <button onClick={() => setTheme(orangeTheme)}>Orange Theme</button>
       <h3>Current Theme: {theme}</h3>
     </>
   );
